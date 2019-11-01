e102d1eba5e0fb042643f1ec002b45c0ecc7bdff
abinpaul1 <abin.paul1@gmail.com>
2019-11-01 23:06:03 +0530

    Ported to mysql

--- hostel_management/settings.py
+++ hostel_management/settings.py
@@ -76,13 +76,24 @@ WSGI_APPLICATION = 'hostel_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# Database for sqlite3
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+#Database for mysql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '~/.my.cnf',
+        },
+        'NAME': 'hostels'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
